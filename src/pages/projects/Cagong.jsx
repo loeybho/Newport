fea3be6216f2867d8e7377bf2a02f24197791275
loeybho <byo_l@naver.com>
2024-06-12 17:24:52 +0900

    Cont. 카공 프로젝트 내용 넣는 중

--- src/pages/projects/Cagong.jsx
+++ src/pages/projects/Cagong.jsx
@@ -23,14 +23,16 @@ export default function Cagong() {
           </ul> */}
         </div>
         <div className="col-span-5 md:col-span-3 sm:col-span-2 bg-white text-customBlack p-10">
-          <h1 className="text-[2rem] font-extrabold mb-4">
-            카공여지도 개발 일지
-          </h1>
+          <h1 className="text-[2.2rem] font-extrabold mb-4">카공여지도</h1>
           <div className="mt-4 mb-4">
             <p className="text-gray-500 text-[1.2rem] ">
               2024.03.28 - 2024.04.25
             </p>
           </div>
+          <div className="p-10">
+            <img src={cagong} className="w-1/2" />
+          </div>
+
           <p className="text-[1.4rem] text-gray-700 leading-[2.6rem]">
             카공여지도는 공부하기 좋은 카페를 추천해주는 웹 어플리케이션입니다.
             멋쟁이사자처럼 프론트엔드 스쿨 수업에서 팀을 구성해 기획, 디자인,
@@ -51,40 +53,62 @@ export default function Cagong() {
               </Link>
             </li>
           </ul>
-          <h2 className="text-[1.8rem] font-extrabold mt-24">
-            저는 이런 걸 구현했어요
-          </h2>
-          <img
-            src="/work/spotify/logo.png"
-            alt="카공여지도 여정"
-            className="my-4"
-          />
-          <ul className="ml-4 list-disc">
-            <li>
-              <p className="text-[1.4rem] text-gray-700 leading-[2.6rem]">
-                홈 페이지 : Swiper로 광고 이미지를 홈 화면에 띄우고, 데이터를
-                불러와 카페 리스트 나열
-              </p>
-            </li>
-            <li>
-              <p className="text-[1.4rem] text-gray-700 leading-[2.6rem]">
-                카페 리스트 + 무한 스크롤 : React Query로 카페 리스트를
-                불러오고, 무한 스크롤로 데이터를 6개씩 불러와 데이터 로딩 속도를
-                개선했습니다.
-              </p>
-            </li>
-            <li>
-              <p className="text-[1.4rem] text-gray-700 leading-[2.6rem]">
-                북마크 + 북마크 리스트 : 특정 유저가 저장한 카페의 동일한 순서가
-                담긴 id 값을 추가, 제거할 수 있는 북마크 기능을 구현하였습니다.
-              </p>
-            </li>
-            <li>
-              <p className="text-[1.4rem] text-gray-700 leading-[2.6rem]">
-                리뷰 리스트 : 저장된 리뷰 리스트를 불러왔습니다.
-              </p>
-            </li>
-          </ul>
+
+          <div>
+            <h2 className="text-[1.8rem] font-extrabold mt-24 mb-10">
+              이런 기능을 구현했어요 (기여도 100%) 👩‍💻
+            </h2>
+
+            <ul className="">
+              <li className="text-[1.4rem] text-gray-700 mb-10 leading-[2.6rem]">
+                <h3 className="font-extrabold">홈 페이지</h3>
+                <img src="" alt="카공여지도 설명" className="my-4" />
+                <ul className="ml-7 list-disc">
+                  <li>
+                    <p className="">상단 광고이미지를 Swiper로 렌더링</p>
+                  </li>
+                </ul>
+              </li>
+              <li className="text-[1.4rem] text-gray-700 mb-10 leading-[2.6rem]">
+                <h3 className="font-extrabold">카페 리스트</h3>
+                <img src="" alt="카페 리스트와 무한 스크롤" className="my-4" />
+                <ul className="ml-7 list-disc">
+                  <li>
+                    <p className="">
+                      React Query를 사용하여 데이터 베이스에 저장된 카페 리스트
+                      렌더링.
+                    </p>
+                  </li>
+                  <li>
+                    <p className="">
+                      무한 스크롤로 데이터를 6개씩 불러와 로딩 속도 개선.
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              <li className="text-[1.4rem] text-gray-700 mb-10 leading-[2.6rem]">
+                <h3 className="font-extrabold">북마크</h3>
+                <img src="" alt="북마크 기능" className="my-4" />
+                <ul className="ml-7 list-disc">
+                  <li>
+                    <p className="">
+                      특정 유저가 저장한 카페의 동일한 순서가 담긴 id 값을 추가,
+                      제거할 수 있는 기능 구현.
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              <li className="text-[1.4rem] text-gray-700 mb-10 leading-[2.6rem]">
+                <h3 className="font-extrabold">리뷰 리스트</h3>
+                <img src="" alt="리뷰 리스트" className="my-4" />
+                <ul className="ml-7 list-disc">
+                  <li>
+                    <p className="">저장된 리뷰 리스트 소환</p>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
