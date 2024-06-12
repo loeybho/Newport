682e0de1edf0a5c07baeb303bdca18ba00221300
loeybho <byo_l@naver.com>
2024-06-12 15:57:08 +0900

    Cont. 카공 프로젝트 추가 중

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,46 +1,64 @@
 import { Link } from "react-router-dom";
-import { aspectRatio } from "@tailwindcss/aspect-ratio";
+import cagong from "@assets/01_cagong.png";
+
+const projects = [
+  {
+    id: 1,
+    title: "카공여지도",
+    description:
+      "공부하기 좋은 카페를 추천해주는 반응형 웹입니다. 멋쟁이사자처럼 프론트엔드 스쿨 수업에서 팀을 구성해 기획, 디자인, 프론트엔드 개발까지 완성했습니다.",
+    imageSrc: cagong,
+    altText: "카공여지도 광고 이미지",
+    techStack: ["React", "Recoil", "Axios", "Swiper", "Styled-Components"],
+    link: "/projects/cagong",
+  },
+];
 
 export default function Projects() {
   return (
     <>
-      <div className="bg-customBlack h-screen p-4">
-        <h1 className="text-4xl text-white font-MarkPros font-bold p-14 pt-4 sm:pt-24 mb-28 sm:text-6xl">
-          Projects
-        </h1>
-        <Link to="/projects/cagong" className="">
-          <div className="flex-none font-NanumSquareNeo sm:flex group">
-            <div className="aspect-square w-full bg-red-100">
-              <img src="이미지 주소" alt="프로젝트 이미지" className="w-full" />
-            </div>
-            <div className="w-full border-b border-slate-50 transition duration-300 ease-in-out p-10 text-white hover:text-customBlack hover:bg-customOrange">
-              <h2 className=" text-[2.4rem] font-extrabold font-semibold mb-4">
-                카공여지도
-              </h2>
-              <p className=" mb-5 text-[1.1rem]">
-                공부하기 좋은 카페를 추천해주는 반응형 웹입니다. 멋쟁이사자처럼
-                프론트엔드 스쿨 수업에서 팀을 구성해 기획, 디자인, 프론트엔드
-                개발까지 완성했습니다.
-              </p>
-              <div className="mb-4">
-                <ul className="flex gap-4">
-                  <li className="border py-1 px-5 rounded-full group-hover:border-customBlack ">
-                    React
-                  </li>
-                  <li className="border py-1 px-5 rounded-full group-hover:border-customBlack">
-                    Styled-Component
-                  </li>
-                  <li className="border py-1 px-5 rounded-full group-hover:border-customBlack">
-                    React
-                  </li>
-                </ul>
+      <div className="bg-customBlack p-4">
+        <div className="pt-28 pl-16">
+          <h1 className="text-[4rem] text-white font-MarkPros font-bold mt-4 sm:mt-24 mb-28 sm:text-[6rem]">
+            Projects
+          </h1>
+        </div>
+        {projects.map((project) => (
+          <Link to={project.link} key={project.id} className="">
+            <div className="font-NanumSquareNeo grid grid-cols-1 sm:grid-cols-[1fr_2fr] group mb-8">
+              <div className="bg-customBeige">
+                <img
+                  src={project.imageSrc}
+                  alt={project.altText}
+                  className="w-full"
+                />
               </div>
-              <div className="h-20 flex items-end">
-                <button className="">클릭하여 자세히 보기 {">"}</button>
+              <div className="border-b border-slate-50 transition duration-300 ease-in-out p-10 text-white hover:text-customBlack hover:bg-customOrange">
+                <h2 className="text-[2.4rem] font-extrabold font-semibold mb-4">
+                  {project.title}
+                </h2>
+                <p className="mb-5 text-[1.1rem]">{project.description}</p>
+                <div className="mb-4">
+                  <ul className="flex gap-4 flex-wrap">
+                    {project.techStack.map((tech, index) => (
+                      <li
+                        key={index}
+                        className="border py-1 px-5 rounded-full group-hover:border-customBlack"
+                      >
+                        {tech}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="h-20 flex items-end">
+                  <button className="underline text-customStrongGreen">
+                    클릭하여 자세히 보기 {">"}
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </>
   );
--- src/pages/projects/Cagong.jsx
+++ src/pages/projects/Cagong.jsx
@@ -1,39 +1,32 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Projects() {
+export default function Cagong() {
   return (
-    <div className="bg-customBlack min-h-screen p-4">
-      <div className="max-w-screen-xl mx-auto grid grid-cols-6 gap-4 md:grid-cols-4 sm:grid-cols-2">
+    <div className="bg-customBeige min-h-screen p-4 text-gray-600 font-medium">
+      <div className=" max-w-screen-xl mx-auto gap-4 grid grid-cols-2 sm:flex">
         <div className="col-span-1 row-span-1 md:row-span-4 p-4">
-          <h3 className="text-white text-lg font-bold">
-            <Link to="/work/#contrib-list">rsms/work</Link>
+          <h3 className="text-lg font-bold">
+            <Link to="/work/#contrib-list">
+              <img src="" alt="" />
+            </Link>
           </h3>
-          <div className="meta text-white mt-4">
-            <p>
-              2006–2010
-              <br />
-              <a href="https://spotify.com/" className="text-blue-400">
-                spotify.com
-              </a>
-            </p>
-          </div>
-          <ul className="pagelist mt-4 text-white">
-            <li className="current mb-2">
-              <Link to="/work/spotify/">Spotify</Link>
+
+          <ul className="pagelist mt-4 ">
+            <li className="current mb-2 underline">
+              <Link to="/work/spotify/">Cagong</Link>
             </li>
-            <li className="mb-2">
-              <Link to="/work/figma/">Figma</Link>
-            </li>
-            <li className="mb-2">
-              <Link to="/work/inter/">Inter</Link>
+            <li className="mb-2 underline">
+              <Link to="/work/figma/">Wanderlends</Link>
             </li>
           </ul>
         </div>
-        <div className="col-span-5 md:col-span-3 sm:col-span-2 bg-white text-black p-4">
+        <div className="col-span-5 md:col-span-3 sm:col-span-2 bg-white text-customBlack p-4">
           <h1 id="my-work-with-spotify" className="text-3xl font-bold mb-4">
             My work with Spotify
           </h1>
+          <div className="mt-4 mb-4">
+            <p className="text-gray-400 font-normal">2024.03.28 - 2024.04.25</p>
+          </div>
           <p>
             I’ve been part of{" "}
             <a href="https://www.spotify.com/" className="text-blue-400">
@@ -53,7 +46,14 @@ export default function Projects() {
             cards, style guides, office architecture, etc) is my work as well as
             the different end user client applications (interaction design).
           </p>
-
+          <ul className="pagelist mt-4 flex gap-3 ">
+            <li className="current mb-2 ">
+              <Link to="/work/spotify/">배포 URL</Link>
+            </li>
+            <li className="mb-2 ">
+              <Link to="/work/figma/">Github</Link>
+            </li>
+          </ul>
           <h2 id="the-spotify-brand" className="text-2xl font-bold mt-8">
             The Spotify brand
           </h2>
@@ -77,176 +77,6 @@ export default function Projects() {
             My busy desk in our first office at Riddargatan 20 in Stockholm:
           </p>
           <img src="/work/spotify/desk.jpg" alt="My desk" className="my-4" />
-
-          <h2 id="desktop-application" className="text-2xl font-bold mt-8">
-            Desktop application
-          </h2>
-          <p>
-            The Spotify desktop application come in a dark grey fashion and is
-            used by millions of people every day, on Mac OS, Windows and Linux.
-            Standard user interface treats like window controls and scrollbars
-            obey the host system while we have taken some
-            semi-conservative-but-still-daring steps for other elements.
-          </p>
-          <img
-            src="/work/spotify/desktopapp.png"
-            alt="Desktop UI Intro"
-            className="my-4"
-          />
-          <p>Hand crafted pixels</p>
-          <img
-            src="/work/spotify/handcrafted-pixels.png"
-            alt="Bits and Pieces"
-            className="my-4"
-          />
-          <p>
-            A dynamic user interface which offers a limited set including the
-            most important features through clearly visible controls (like play,
-            skip track, search, sharing, etc), while providing more advanced
-            users with slightly less common functionality — e.g. resizing views,
-            jumping to playing context, queueing tracks, and so on.
-          </p>
-          <img
-            src="/work/spotify/desktopui.png"
-            alt="More bits and Pieces"
-            className="my-4"
-          />
-          <p>
-            We also tried to keep settings to a minimum with thought-through
-            &amp; sensible defaults.
-          </p>
-          <img
-            src="/work/spotify/hovering-art-directors.jpg"
-            alt="And on the seventh day they invented digital beer"
-            className="my-4"
-          />
-          <p>
-            Pictured above: Per, Mattias &amp; Me stretching out some Mac OS
-            interface issues.
-          </p>
-
-          <h2 id="mobile-applications" className="text-2xl font-bold mt-8">
-            Mobile applications
-          </h2>
-          <p>
-            Since Spotify is a service “in the cloud” and it’s all about music,
-            people need to access Spotify wherever they are — on a bike,
-            traveling abroad or just walking around in the city. We created
-            Spotify client applications for a number of different devices and
-            platforms including Apple iOS (i.e. iPhone), Android and Nokia S60.
-          </p>
-          <img
-            src="/work/spotify/mobile-devices.jpg"
-            alt="Mobile devices"
-            className="my-4"
-          />
-
-          <h2 id="design-process" className="text-2xl font-bold mt-8">
-            Design process
-          </h2>
-          <p>
-            I’m the kind of designer who place myself closer to “architect” than
-            “artist” in the spectrum of art–science ratio. Sketching on good old
-            paper with a graphite pencil beats all (early-stages) prototyping
-            software I’ve tried. It’s easy to interact with and everyone can
-            join in.
-          </p>
-          <img
-            src="/work/spotify/ui-sketch.jpg"
-            alt="iPhone paper sketch"
-            className="my-4"
-          />
-          <p>
-            Marrying function with form is a natural part of human–machine
-            interfaces, thus embedding notes on functionality and form when
-            sketching out the rough flow comes natural and saves on bureaucracy.
-          </p>
-          <img
-            src="/work/spotify/ui-sketch2.jpg"
-            alt="iPhone paper sketch"
-            className="my-4"
-          />
-          <img
-            src="/work/spotify/hovering-ceo.jpg"
-            alt="Daniel and Me going through some desktop app UI flow ideas"
-            className="my-4"
-          />
-          <p>
-            Keeping all stakeholders in the loop during the early stages of the
-            design process is crucial.
-          </p>
-          <p>Later moving on with computer aided sketching and prototyping.</p>
-          <img
-            src="/work/spotify/flow-spreads.jpg"
-            alt="iPhone dimension specs"
-            className="my-4"
-          />
-          <p>
-            Communicating component dimensions, colors, fonts, etc to the
-            engineers was often done through simple print-outs.
-          </p>
-          <img src="/work/spotify/app-resources.png" alt="" className="my-4" />
-          <p>
-            Most graphic resources like icons and controls where first sketched
-            on paper, then drawn as vector-based computer images and finally
-            visualized by means of hand-crafted pixels, ending up as PNG image
-            data.
-          </p>
-
-          <h2 id="culture" className="text-2xl font-bold mt-8">
-            Culture
-          </h2>
-          <p>
-            The Spotify culture has always played an important role in the
-            (internal) success of Spotify. Ever since day one there have been
-            that “happy start-up” feeling where mottos like “if we have fun we
-            will produce good stuff” fit in.
-          </p>
-          <img
-            src="/work/spotify/alpha-release-group-photo.jpg"
-            alt="Spotify 0.1.0"
-            className="my-4"
-          />
-          <p>
-            <em>Above</em> Here we are in 2007. We had just finished the very
-            first release of Spotify to which friends and family where given
-            access. In contrast; three years later there are more than 200
-            people employed by Spotify in seven different countries.
-          </p>
-          <img
-            src="/work/spotify/bobbie-and-sophia.jpg"
-            alt="Behave!"
-            className="my-4"
-          />
-          <p>
-            Roberta and Sophia, probably plotting{" "}
-            <a
-              href="http://www.independent.co.uk/life-style/gadgets-and-tech/features/tim-walker-like-a-charity-mugger-roberta-from-spotify-is-secretly-after-my-money-1667721.html"
-              className="text-blue-400"
-            >
-              “Roberta from Spotify”
-            </a>
-            .
-          </p>
-          <img
-            src="/work/spotify/berlin.jpg"
-            alt="Code shall be written with style"
-            className="my-4"
-          />
-          <p>Code, code, code and dance music.</p>
-          <img
-            src="/work/spotify/bbq.jpg"
-            alt="You look sooo Scottish with those glasses"
-            className="my-4"
-          />
-          <p>
-            <a
-              href="https://www.flickr.com/photos/rsms/tags/spotify/"
-              className="text-blue-400"
-            >
-              More photos from the early and late life of Spotify →
-            </a>
-          </p>
         </div>
       </div>
     </div>
